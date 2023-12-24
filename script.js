489e4120d50efb3e3872a607c72755992cf7c84b
“markclift” <mark.clift.uk@gmail.com>
2023-12-24 15:45:39 +0000

    update

--- script.js
+++ script.js
@@ -6,82 +6,85 @@ let isGameOver = false;
 let spawnInterval, scoreInterval; // Variables to hold intervals
 
 function setInitialPlayerPosition() {
-    playerX = gameArea.offsetWidth / 2 - player.offsetWidth / 2;
-    playerY = gameArea.offsetHeight / 2 - player.offsetHeight / 2;
-    player.style.left = playerX + 'px';
-    player.style.top = playerY + 'px';
+  playerX = gameArea.offsetWidth / 2 - player.offsetWidth / 2;
+  playerY = gameArea.offsetHeight / 2 - player.offsetHeight / 2;
+  player.style.left = playerX + 'px';
+  player.style.top = playerY + 'px';
 }
 
 document.addEventListener('DOMContentLoaded', setInitialPlayerPosition); // Set position on page load
 
 document.addEventListener('keydown', (e) => {
-    const speed = 10; // Increased speed for faster movement
-    switch (e.key) {
-        case 'ArrowLeft':
-            playerX = Math.max(0, playerX - speed);
-            break;
-        case 'ArrowRight':
-            playerX = Math.min(gameArea.offsetWidth - player.offsetWidth, playerX + speed);
-            break;
-        case 'ArrowUp':
-            playerY = Math.max(0, playerY - speed);
-            break;
-        case 'ArrowDown':
-            playerY = Math.min(gameArea.offsetHeight - player.offsetHeight, playerY + speed);
-            break;
-    }
-    player.style.left = playerX + 'px';
-    player.style.top = playerY + 'px';
+  const speed = 10; // Increased speed for faster movement
+  switch (e.key) {
+    case 'ArrowLeft':
+      playerX = Math.max(0, playerX - speed);
+      break;
+    case 'ArrowRight':
+      playerX = Math.min(gameArea.offsetWidth - player.offsetWidth, playerX + speed);
+      break;
+    case 'ArrowUp':
+      playerY = Math.max(0, playerY - speed);
+      break;
+    case 'ArrowDown':
+      playerY = Math.min(gameArea.offsetHeight - player.offsetHeight, playerY + speed);
+      break;
+  }
+  player.style.left = playerX + 'px';
+  player.style.top = playerY + 'px';
 });
 
 function createFootball() {
-    const football = document.createElement('div');
-    football.classList.add('football');
-    gameArea.appendChild(football);
+  const football = document.createElement('div');
+  football.classList.add('football');
+  gameArea.appendChild(football);
 
-    const footballSize = 30; // Fixed size for all footballs
-    football.style.width = football.style.height = footballSize + 'px';
+  const footballSize = 30; // Fixed size for all footballs
+  football.style.width = football.style.height = footballSize + 'px';
 
-    const startPosition = Math.random() * (gameArea.offsetHeight - footballSize);
-    football.style.top = startPosition + 'px';
-    
-    let footballX = gameArea.offsetWidth;
-    function moveFootball() {
-        footballX -= 2 + difficulty; // Increase speed based on difficulty
-        football.style.left = footballX + 'px';
+  const startPosition = Math.random() * (gameArea.offsetHeight - footballSize);
+  football.style.top = startPosition + 'px';
 
-        // Collision detection
-        if (!isGameOver && footballX < playerX + player.offsetWidth &&
-            footballX + football.offsetWidth > playerX &&
-            startPosition < playerY + player.offsetHeight &&
-            startPosition + football.offsetHeight > playerY) {
-                isGameOver = true;
-                alert('Game Over! Score: ' + score);
-                resetGame();
-        }
+  let footballX = gameArea.offsetWidth;
 
-        if (footballX > 0) {
-            requestAnimationFrame(moveFootball);
-        } else {
-            football.remove();
-        }
+  function moveFootball() {
+    if (isGameOver) {
+      football.remove();
+      return; // Stop moving if the game is over
     }
-    moveFootball();
+
+    footballX -= 2 + difficulty;
+    football.style.left = footballX + 'px';
+
+    if (!isGameOver && footballX < playerX + player.offsetWidth &&
+      footballX + football.offsetWidth > playerX &&
+      startPosition < playerY + player.offsetHeight &&
+      startPosition + football.offsetHeight > playerY) {
+      isGameOver = true;
+      alert('Game Over! Score: ' + score);
+      setTimeout(resetGame, 100); // Add a short delay before resetting
+    } else if (footballX > 0) {
+      requestAnimationFrame(moveFootball);
+    } else {
+      football.remove();
+    }
+  }
+  moveFootball();
 }
 
 function resetGame() {
-    clearInterval(spawnInterval); // Clear existing interval
-    clearInterval(scoreInterval); // Clear existing score interval
+  clearInterval(spawnInterval);
+  clearInterval(scoreInterval);
 
-    document.querySelectorAll('.football').forEach(football => football.remove());
-    score = 0;
-    difficulty = 1;
-    document.getElementById('score').innerText = score;
-    document.getElementById('difficulty').innerText = difficulty; // Update difficulty display
-    setInitialPlayerPosition(); // Reset player position
-    isGameOver = false;
+  document.querySelectorAll('.football').forEach(football => football.remove());
+  score = 0;
+  difficulty = 1;
+  document.getElementById('score').innerText = score;
+  document.getElementById('difficulty').innerText = difficulty; // Update difficulty display
+  setInitialPlayerPosition(); // Reset player position
+  isGameOver = false;
 
-    startGame(); // Restart game mechanics
+  startGame(); // Restart game mechanics
 }
 
 function updateDifficulty() {
@@ -95,15 +98,15 @@ setInterval(updateDifficulty, 10000); // Increase difficulty every 10 seconds
 
 function spawnFootball() {
   if (!isGameOver) {
-      createFootball();
+    createFootball();
   }
 }
 
 function startGame() {
   spawnInterval = setInterval(spawnFootball, 2000); // Start with a fixed interval
   scoreInterval = setInterval(() => {
-      score++;
-      document.getElementById('score').innerText = score;
+    score++;
+    document.getElementById('score').innerText = score;
   }, 1000);
 }
 
